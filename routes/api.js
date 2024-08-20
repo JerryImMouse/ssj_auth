0bb3052f9a8b7b2f9590067edaf2ed4bdb95f2b1
JerryImMouse <toperkas@gmail.com>
2024-08-20 11:45:07 +0500

    cache manager, and more env vars

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 const database = require("../database/sqlite");
 const logger = require("../utilities/logger");
-const {discordLinkTemplate, clientId, redirectUri} = require("../configuration/config");
+const {discordLinkTemplate, clientId, redirectUri, use_caching, cache_size, use_given_table} = require("../configuration/config");
 const dHelper = require("../utilities/discordhelper");
 const {checkAccess} = require("../utilities/tokenutils");
 const path = require("path");
+const CacheManager = require("../utilities/cache_managing");
 const {setGivenTo, getGivenBySS14Id, setGivenToZeroAll} = require("../database/sqlite");
+
+const userCache = new CacheManager(cache_size);
 
 router.get("/check", async (req, res) => {
     if (!req.query.api_token)
@@ -18,6 +21,15 @@ router.get("/check", async (req, res) => {
         return res.status(400).json({ error: "No user id provided" });
     }
 
+    if (use_caching) {
+        const user = userCache.get(req.query.userid);
+        if (user != null) {
+            logger.info(`Found user cache for ${req.query.userid}`);
+            const { id, access_token, refresh_token, ...safeUser } = user;
+            return res.status(200).json(safeUser);
+        }
+    }
+
     try
     {
         const user = await database.getUserBySS14Id(req.query.userid);
@@ -25,7 +37,9 @@ router.get("/check", async (req, res) => {
             return res.status(404).json({ error: "No user found" });
         }
 
-        return res.status(200).json(user);
+        userCache.set(req.query.userid, user);
+        const { id, access_token, refresh_token, ...safeUser } = user;
+        return res.status(200).json(safeUser);
     }
     catch (error)
     {
@@ -97,6 +111,9 @@ router.post('/given', async (req, res) => {
     if (!checkAccess(req.query.api_token))
         return res.status(401).sendFile(path.join(__dirname, '..', 'public', 'html', 'unauthorized.html'));
 
+    if (!use_given_table)
+        return res.status(405).send("Given table is turned off")
+
     if (!req.query.userid) {
         return res.status(400).json({ error: "No user ID provided" });
     }
@@ -119,6 +136,9 @@ router.get('/is_given', async (req, res) => {
     if (!checkAccess(req.query.api_token))
         return res.status(401).sendFile(path.join(__dirname, '..', 'public', 'html', 'unauthorized.html'));
 
+    if (!use_given_table)
+        return res.status(405).send("Given table is turned off")
+
     if (!req.query.userid) {
         return res.status(400).json({ error: "No user ID provided" });
     }
@@ -137,6 +157,9 @@ router.post('/wipe_given', async (req, res) => {
 
     if (!checkAccess(req.query.api_token))
         return res.status(401).sendFile(path.join(__dirname, '..', 'public', 'html', 'unauthorized.html'));
+
+    if (!use_given_table)
+        return res.status(405).send("Given table is turned off")
 
     await setGivenToZeroAll();
 
---
+++ utilities/cache_managing.js
@@ -0,0 +1,64 @@
+const {cache_update_timeout} = require('../configuration/config')
+const logger = require('./logger')
+
+class CacheManager {
+    constructor(maxSize = 5) {
+        this.cacheMap = {};
+        this.order = [];
+        this.maxSize = maxSize;
+
+        setInterval(() => {
+            this.clear();
+            logger.info('CacheManager cache cleared');
+        }, cache_update_timeout * 1000);
+    }
+
+    set(key, value) {
+        if (this.cacheMap[key]) {
+            const index = this.order.indexOf(key);
+            if (index > -1) {
+                this.order.splice(index, 1);
+            }
+        }
+
+        if (this.order.length >= this.maxSize) {
+            const oldestKey = this.order.shift();
+            delete this.cacheMap[oldestKey];
+        }
+
+        this.order.push(key);
+        this.cacheMap[key] = { value };
+    }
+
+    get(key) {
+        const cacheItem = this.cacheMap[key];
+        return cacheItem ? cacheItem.value : null;
+    }
+
+    delete(key) {
+        delete this.cacheMap[key];
+        const index = this.order.indexOf(key);
+        if (index > -1) {
+            this.order.splice(index, 1);
+        }
+    }
+
+    clear() {
+        this.cacheMap = {};
+        this.order = [];
+    }
+
+    setMaxSize(newSize) {
+        this.maxSize = newSize;
+        while (this.order.length > this.maxSize) {
+            const oldestKey = this.order.shift();
+            delete this.cacheMap[oldestKey];
+        }
+    }
+
+    keys() {
+        return this.order.slice();
+    }
+}
+
+module.exports = CacheManager;
