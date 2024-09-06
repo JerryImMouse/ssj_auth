dd93d32139c2dabe947fad81c31a07e50ecab675
JerryImMouse <toperkas@gmail.com>
2024-09-06 17:13:26 +0500

    add status codes

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ router.get('/callback', async (req, res) => {
     logger.info(`GET /callback. Got callback with code: ${req.query.code}`);
 
     if (!req.query.state) {
-        res.sendFile(path.join(__dirname, '..', 'public', 'html', 'error.html'));
+        res.status(400).sendFile(path.join(__dirname, '..', 'public', 'html', 'error.html'));
         return;
     }
     const base64Userid = req.query.state;
@@ -18,20 +18,20 @@ router.get('/callback', async (req, res) => {
     const tokenData = await exchangeCode(req.query.code);
     if (!tokenData) {
         logger.error("Unable to get token");
-        res.sendFile(path.join(__dirname, '..', 'public', 'html', 'error.html'));
+        res.status(500).sendFile(path.join(__dirname, '..', 'public', 'html', 'error.html'));
         return;
     }
 
     const userObject = await getDiscordIdentifyScopeUnsafe(tokenData['access_token']);
     if (!userObject) {
         logger.error("Unable to get identify scope");
-        res.sendFile(path.join(__dirname, '..', 'public', 'html', 'error.html'));
+        res.status(500).sendFile(path.join(__dirname, '..', 'public', 'html', 'error.html'));
         return;
     }
 
     const fetched = await getUserByDiscordId(userObject.user.id);
     if (fetched) {
-        res.sendFile(path.join(__dirname, '..', 'public', 'html', 'error.html'));
+        res.status(409).sendFile(path.join(__dirname, '..', 'public', 'html', 'error.html'));
         logger.info(`Declining already existed auth entry from ${userObject.user.id} | ${userid}`);
         return;
     }
@@ -50,11 +50,11 @@ router.get('/callback', async (req, res) => {
     const result1 = await insertGivenUser(userObject.user.id, userid, 0);
 
     if (result && result1) {
-        res.sendFile(path.join(__dirname, '..', 'public', 'html', 'success.html'));
+        res.status(200).sendFile(path.join(__dirname, '..', 'public', 'html', 'success.html'));
         return;
     }
 
-    res.sendFile(path.join(__dirname, '..', 'public', 'html', 'error.html'));
+    res.status(500).sendFile(path.join(__dirname, '..', 'public', 'html', 'error.html'));
 });
 
 module.exports = router;
