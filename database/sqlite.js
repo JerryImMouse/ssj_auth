9dbbff8f05e5e4f20800ffb51eb83ec5743a3c35
JerryImMouse <toperkas@gmail.com>
2024-10-21 18:31:11 +0500

    okay, now we are supporting docker fully

--- database/sqlite.js
+++ database/sqlite.js
@@ -1,8 +1,14 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const fs = require('fs');
 const logger = require('../utilities/logger.js');
 
 const dbPath = path.resolve(__dirname, '..', 'assets', 'database.db');
+const dbDir = path.dirname(dbPath);
+
+if (!fs.existsSync(dbDir)) {
+    fs.mkdirSync(dbDir, { recursive: true });
+}
 
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
